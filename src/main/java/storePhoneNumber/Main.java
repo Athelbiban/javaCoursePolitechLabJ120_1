d583c235e568cd0a87a9d61ade0e4e0835eeb6da
athelbiban <vostrovso@gmail.com>
2023-11-29 21:56:28 +0300

    add: equals-hashCode

--- src/main/java/storePhoneNumber/Main.java
+++ src/main/java/storePhoneNumber/Main.java
@@ -1,11 +1,20 @@
 package storePhoneNumber;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         StorePhoneNumber number1 = new StorePhoneNumber(981, 8567753);
         StorePhoneNumber number2 = new StorePhoneNumber(4982, 627415);
         System.out.println(number1);
         System.out.println(number2);
+        System.out.println();
 
+        Set<StorePhoneNumber> setPhoneNumber = new HashSet<>();
+        setPhoneNumber.add(number1);
+        setPhoneNumber.add(number2);
+        setPhoneNumber.add(new StorePhoneNumber(812, 6161273));
+        System.out.println(setPhoneNumber);
     }
 }
--- src/main/java/storePhoneNumber/StorePhoneNumber.java
+++ src/main/java/storePhoneNumber/StorePhoneNumber.java
@@ -1,5 +1,7 @@
 package storePhoneNumber;
 
+import java.util.Objects;
+
 public class StorePhoneNumber {
     private Integer regionCode;
     private Integer phoneNumber;
@@ -27,6 +29,20 @@ public class StorePhoneNumber {
         this.phoneNumber = phoneNumber;
     }
 
+    // контракт equals-hashCode для обеспечения использования с множествами и ассоциативными массивами
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof StorePhoneNumber that)) return false;
+        return Objects.equals(regionCode, that.regionCode) && Objects.equals(phoneNumber, that.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(regionCode, phoneNumber);
+    }
+
+    // реализовать особый toString()
     @Override
     public String toString() {
         int lengthFirstGroup;
@@ -34,17 +50,10 @@ public class StorePhoneNumber {
 
         String stringPhoneNumber = phoneNumber + "";
         int lengthPhoneNumber = stringPhoneNumber.length();
-        if (lengthPhoneNumber % 2 == 0) {
-            lengthFirstGroup = 2;
-        }
-        else {
-            lengthFirstGroup = 3;
-        }
+        lengthFirstGroup = lengthPhoneNumber % 2 == 0 ? 2 : 3;
 
         StringBuilder temp = new StringBuilder("(");
-        temp.append(regionCode).append(")");
-        String firstGroup = stringPhoneNumber.substring(0, lengthFirstGroup);
-        temp.append(firstGroup);
+        temp.append(regionCode).append(")").append(stringPhoneNumber, 0, lengthFirstGroup);
 
         for (int i = 1; i < lengthPhoneNumber / LENGTHSECONDGROUP; i++) {
             temp.append("-").append(stringPhoneNumber, lengthFirstGroup, LENGTHSECONDGROUP + lengthFirstGroup);

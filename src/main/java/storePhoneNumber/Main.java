54d3d363ce11cf6d2d54e5df7f965d14b1dd29c2
athelbiban <vostrovso@gmail.com>
2023-11-30 16:03:38 +0300

    upd

--- src/main/java/storePhoneNumber/Main.java
+++ src/main/java/storePhoneNumber/Main.java
@@ -7,14 +7,20 @@ public class Main {
     public static void main(String[] args) {
         StorePhoneNumber number1 = new StorePhoneNumber(981, 8567753);
         StorePhoneNumber number2 = new StorePhoneNumber(4982, 627415);
+        StorePhoneNumber number3 = new StorePhoneNumber(981, 8567753);
         System.out.println(number1);
         System.out.println(number2);
+        System.out.println(number3);
         System.out.println();
 
         Set<StorePhoneNumber> setPhoneNumber = new HashSet<>();
         setPhoneNumber.add(number1);
         setPhoneNumber.add(number2);
         setPhoneNumber.add(new StorePhoneNumber(812, 6161273));
+        System.out.println(setPhoneNumber);
+
+        System.out.println(number1.equals(number3));
+        setPhoneNumber.add(number3); // номер не будет добавлен т.к. уже есть в множестве
         System.out.println(setPhoneNumber);
     }
 }
